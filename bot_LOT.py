fb05bb49f8607b9c2d384835fb1e4207b96301b1
ABC <mouse_i@tut.by>
2023-02-24 21:21:17 +0300

    Попытка сделать чет толковое =)

--- bot_LOT.py
+++ bot_LOT.py
@@ -1,74 +1,110 @@
-import telebot
+from telegram_bot_calendar import DetailedTelegramCalendar, LSTEP
 from telebot.types import InputMediaPhoto
-import json
 
+import json
+import random
+import telebot
 
 bot = telebot.TeleBot('5790530956:AAHR5gW3d7Z7K1NWj81Ycej8zf4rHYUYhr4')
 
 
-lsit_buf_photo = []
+
+list_id_lots = []
 
 lot = {}
 
-# lot = {
-#     "id_admin": "id",
-#     "mas_photos": ["photo1", "photo2","photo1", "photo2","photo1", "photo2","photo1"],
-#     "description": "описание",
-#     "city" : "город",
-#     "delivery": "условия доставки",
-#     "cost": "цена"
-# }
+
+lst_id_lot = []
+
+def gen_id(lst):
+    point = 0
+    while point == 0:
+        id = random.randint(1,99999)
+        if id not in lst:
+            lst.append(id)
+            point +=1
+            return id
+
+
+
+
+'''
+lot = {
+    "id_admin": "id", +
+    "id_lot": "id_lot",-
+    "mas_photos": ["photo1", "photo2"],
+    "description": "описание",
+    "city" : "город",
+    "delivery": "условия доставки",
+    "cost": "цена",
+    "start_time": "старт публикации",
+    "finish_time": "конец аукциона"
+}
+'''
+
+
+def add_time(message):
+    calendar, step = DetailedTelegramCalendar().build()
+
+    bot.send_message(message.chat.id, f"Select {LSTEP[step]}", reply_markup=calendar)
 
 def add_cost(message):
-    if  len(message.text) <= 1 or message.content_type != "text":
+    if message.content_type == "text" and len(message.text) > 1:
+        lot["cost"] = message.text
+        id_lots = gen_id(list_id_lots)
+        lot["id_lot"] = id_lots
+        with open(f'{id_lots}.json', 'w', encoding='utf-8') as f:
+            json.dump(lot, f, ensure_ascii=False, indent=4)
+
+        bot.send_message(message.from_user.id, "Покачто лот сформирован")
+
+        # add_time(message)
+        # for x,y in lot.items():
+        #     print(x,"  ",y)
+    else:
         msg = bot.send_message(message.from_user.id, "Что-то пошло не так, попробуйте еще раз")
         bot.register_next_step_handler(msg, add_cost)  
-    else:
-        lot["cost"] = message.text
-        bot.send_message(message.from_user.id, "Лот успешно сформирован!")
-        for x,y in lot.items():
-            print(x,"  ",y)
 
 def add_delivery(message):
-    if  len(message.text) <= 1 or message.content_type != "text":
-        msg = bot.send_message(message.from_user.id, "Что-то пошло не так, попробуйте еще раз")
-        bot.register_next_step_handler(msg, add_delivery)
-    else:
+    if message.content_type == "text" and len(message.text) > 1:
         lot["delivery"] = message.text
         msg = bot.send_message(message.from_user.id, "Укажите стартовую цену лота")
         bot.register_next_step_handler(msg, add_cost)
+    else:
+        msg = bot.send_message(message.from_user.id, "Что-то пошло не так, попробуйте еще раз")
+        bot.register_next_step_handler(msg, add_delivery)
 
 
 def add_city(message):
-    if  len(message.text) <= 1 or message.content_type != "text":
-        msg = bot.send_message(message.from_user.id, "Что-то пошло не так, попробуйте еще раз")
-        bot.register_next_step_handler(msg, add_city)   
-    else:
+    if message.content_type == "text" and len(message.text) > 1:
         lot["city"] = message.text
         msg = bot.send_message(message.from_user.id, "Условия доставки? (Почта /Самовывоз /Курьер)")
         bot.register_next_step_handler(msg, add_delivery)
+    else:
+        msg = bot.send_message(message.from_user.id, "Что-то пошло не так, попробуйте еще раз")
+        bot.register_next_step_handler(msg, add_city)   
+
+
+# len(message.text) <= 1 or 
 
 def add_description(message):
     print(message.content_type)
-    if  len(message.text) <= 1 or message.content_type != "text":
-        msg = bot.send_message(message.from_user.id, "Что-то пошло не так, попробуйте еще раз")
-        bot.register_next_step_handler(msg, add_description)
-
-    else:
+    if message.content_type == "text" and len(message.text) > 1:
         lot["description"] = message.text
         msg = bot.send_message(message.from_user.id, " Отличто! Описание добавлено. Укажите местонахождение (Город)")
         bot.register_next_step_handler(msg, add_city)
-
+    else:
+        msg = bot.send_message(message.from_user.id, "Что-то пошло не так, попробуйте еще раз")
+        bot.register_next_step_handler(msg, add_description)
     
 
-def Add_photo(message):
-    print(message.content_type)
+def add_photo(message):
     if "id_admin" not in lot:
         bot.send_message(message.from_user.id, " Если хотите добавить лот, введите команду '/add_lot'")
 
     elif message.content_type !="photo" :
         msg = bot.send_message(message.from_user.id, "Что-то пошло не так, попробуйте всетаки загрузить картинку ")
-        bot.register_next_step_handler(msg, Add_photo)
+        bot.register_next_step_handler(msg, add_photo)
 
     elif  message.content_type =="photo":
         lot["mas_photos"] = []
@@ -79,40 +115,12 @@ def Add_photo(message):
         bot.register_next_step_handler(msg, add_description)
 
 
-# @bot.message_handler(content_types= ["photo"])
-# def Add_photo(message):
-#     # print(message.content_type)
-#     # print(message)
-#     # сдесь будет проверка на права Админа
-#     if message.content_type !="photo" :
-#         msg = bot.send_message(message.from_user.id, "Что-то пошло не так, попробуйте всетаки загрузить картинку ")
-#         bot.register_next_step_handler(msg, Add_photo)
-
-#     elif len(lot["mas_photos"]) >= 8:
-#         bot.send_message(message.from_user.id, "Достигнут лемит по количеству картинок")
-#         msg = bot.send_message(message.from_user.id, "Пойдем дальше. Теперь добавьте описание ")
-#         bot.register_next_step_handler(msg, add_description)
-
-
-#     elif message.content_type =="photo" and len(lot["mas_photos"]) <= 8 :
-#         lot["mas_photos"].append(message.photo[-1].file_id)
-
-
-#         msg = bot.send_message(message.from_user.id, "Хорошо! Теперь добавьте описание ")
-#         bot.register_next_step_handler(msg, add_description)
-    
-        
-#     print(lot)
-
-
-
 @bot.message_handler(content_types= ["text"])
 def any_text(message):
-    print(len(message.text))
- 
+
     if message.text == '/start':
         bot.send_message(message.from_user.id, 'Добро пожаловать ✌🏻\n\nСкоро я начну работать')
-
+        
     elif message.text == "":
         bot.send_message(message.from_user.id, 'Для начала работы бота необходимо прописать команду /start')
 
@@ -120,7 +128,7 @@ def any_text(message):
         # сдесь будет проверка на права Админа
         lot["id_admin"] = message.from_user.id
         msg = bot.send_message(message.from_user.id, "Для началла пришлите не мене фото (не более 8шт.)")
-        bot.register_next_step_handler(msg, Add_photo)
+        bot.register_next_step_handler(msg, add_photo)
             
 
         
@@ -131,7 +139,17 @@ def any_text(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def answer(call):
-    pass
+    result, key, step = DetailedTelegramCalendar().process(call.data)
+    if not result and key:
+        bot.edit_message_text(f"Select {LSTEP[step]}",
+                              call.message.chat.id,
+                              call.message.message_id,
+                              reply_markup=key)
+    elif result:
+
+        bot.edit_message_text(f"You selected {result}",
+                              call.message.chat.id,
+                              call.message.message_id)
 
 
 
--- test.py
+++ test.py
@@ -1,10 +1,18 @@
+import random
+lst_id_lot = []
+
+def gen_id(lst):
+    point = 0
+    while point == 0:
+        id = random.randint(1,99999)
+        if id not in lst:
+            lst.append(id)
+            point +=1
+            return id
 
 
 
-dct = {}
-dct['id'] = 123
-dct['mass_id'] = [1,12,3]
 
-if 'idd' in dct:
+print(gen_id(lst_id_lot))
 
-    print(True)
+
