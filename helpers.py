c1505c8885ecf0e5283124077688071d4ae6bf1c
kirilgoloveiko <snikerz.kg@gmail.com>
2023-02-25 11:58:57 +0300

    Добавлена функция формирования карточки(лота)

--- helpers.py
+++ helpers.py
@@ -1,14 +1,38 @@
+import json
 import random
 
-d = {1329081957: {'Описание': 'Описание лота', 'Город': 'Минск'}}
+from telebot.types import InputMediaPhoto
 
 
-def card(d: dict):
-    text = f'Описание: <b>{d["Описание"]}</b>\n' \
-           f'Город: <b>{d["Город"]}</b>\n'
-    return text
+def gen_caption(json_file):
+    with open(json_file, 'r', encoding='utf-8') as f:
+        data_from_json = json.load(f)
 
-# print(card(d))
+    caption = f"{data_from_json['description']}\n"\
+                  f"{data_from_json['city']}\n"\
+                  f"{data_from_json['delivery']}\n"
+    return caption
+
+
+def card(json_file):
+    with open(json_file, 'r', encoding='utf-8') as f:
+        data_from_json = json.load(f)
+
+    media_list = []
+
+    for photo in data_from_json['mas_photos']:
+        if not media_list:
+            obj = InputMediaPhoto(photo, caption=gen_caption(json_file))
+            media_list.append(obj)
+        else:
+            obj = InputMediaPhoto(photo)
+            media_list.append(obj)
+
+    return media_list
+
+
+# print(card('38938.json'))
+
 
 def gen_lot_id() -> int:
     numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
